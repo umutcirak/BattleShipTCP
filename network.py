7b731e37791972066938423ab14d06a7883f344b
umutcirak <umut.cirak@ozu.edu.tr>
2022-03-17 05:38:27 +0300

    TCP connection is builded.

---
+++ network.py
@@ -0,0 +1,24 @@
+import socket
+import pickle
+
+class Network:      # iletişim sınıfı alma ve bağlanma verileri burada tanımlanıyor
+    def __init__(self):
+        self.client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server = "127.0.0.1"
+        self.port = 34000
+        self.addr = (self.server, self.port)
+        self.connection = self.connect()
+    
+    def connect(self):
+        try:
+            self.client.connect(self.addr)
+            return pickle.loads(self.client.recv(2048))
+        except socket.error as error:
+            print(error)
+        
+    def send(self, data):
+        try:
+            self.client.send(pickle.dumps(data))
+            return pickle.loads(self.client.recv(2048))
+        except socket.error as error:
+            print(error)
